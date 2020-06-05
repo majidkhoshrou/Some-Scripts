f101d63e4d75bb99c6fef994f1eeaba4f4c60332
majidkhoshrou <majid.khoshrou@gmail.com>
2020-06-05 14:04:33 +0200

    event hub updated

--- eventhub.py
+++ eventhub.py
@@ -57,6 +57,35 @@ except KeyboardInterrupt:
 
 
 
+from azure.eventhub import EventHubProducerClient
+from azure.eventhub import EventHubProducerClient, EventData
+
+# this scripts simulates production of events for 10 devices
+devices = []
+for x in range(0, 10):
+    devices.append(str(uuid.uuid4()))
+
+# create a producer client to produce/publish events to the event hub
+producer = EventHubProducerClient.from_connection_string(conn_str="Endpoint=sb://mbv-eventhubs.servicebus.windows.net/;SharedAccessKeyName=***;SharedAccessKey=***=", eventhub_name="***")
+
+
+for y in range(0,20):    # for each device, produce 20 events 
+    event_data_batch = producer.create_batch() # create a batch. you will add events to the batch later. 
+    for dev in devices:
+        # create a dummy reading
+        reading = {'id': dev, 'timestamp': str(datetime.datetime.utcnow()), 'uv': random.random(), 'temperature': random.randint(70, 100), 'humidity': random.randint(70, 100)}
+        s = json.dumps(reading) # convert reading into a JSON string
+        event_data_batch.add(EventData(s)) # add event data to the batch
+    producer.send_batch(event_data_batch) # send the batch of events to the event hub
+
+# close the producer    
+producer.close()
+
+
+
+
+
+
 
 
 
